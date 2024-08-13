5b19555d3ac95a9fdee298788f47814629cd252a
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2024-08-13 12:48:27 +0200

    set up category component

--- src/app/layout/navbar/category/category.component.ts
+++ src/app/layout/navbar/category/category.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CategoryService } from './category.service';
+import { Category } from './category.model';
+import { FaIconComponent, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [],
+  imports: [
+    FaIconComponent,
+    FontAwesomeModule,
+    CommonModule
+  ],
   templateUrl: './category.component.html',
   styleUrl: './category.component.scss'
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit{
+
+  categoryService= inject(CategoryService); 
+
+  categories: Category[] | undefined;
+
+  currentActivatedCategory = this.categoryService.getCategoryByDefault();
+
+  ngOnInit():void {
+    this.fetchCategories();
+  }
+
+  private fetchCategories(){
+    this.categories = this.categoryService.getCategories();
+  }
 
 }
