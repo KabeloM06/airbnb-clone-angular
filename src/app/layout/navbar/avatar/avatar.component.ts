7e24a33ebe27f07da58f612e79d5d8d58caaefe7
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2024-08-13 07:35:38 +0200

    set up avatar component

--- src/app/layout/navbar/avatar/avatar.component.ts
+++ src/app/layout/navbar/avatar/avatar.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { FaIconComponent, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-avatar',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FaIconComponent,
+    FontAwesomeModule
+  ],
   templateUrl: './avatar.component.html',
   styleUrl: './avatar.component.scss'
 })
 export class AvatarComponent {
+
+  imageUrl = input<string>();
+  avatarSize = input<"avatar-sm" | "avatar-xl">();
 
 }
